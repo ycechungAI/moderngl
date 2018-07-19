9731f2fd0a809cda4ae8ba7cd7a5f55f2f7c51fb
Szabolcs Dombi <cprogrammer1994@gmail.com>
2018-07-19 07:04:22 +0300

    document load_modules

--- moderngl/mgl/internal/modules.cpp
+++ moderngl/mgl/internal/modules.cpp
@@ -2,6 +2,10 @@
 
 PyObject * moderngl;
 
+/* Loading external modules.
+ * This function must not be called on import.
+ * Circular imports are not supported in this case.
+ */
 bool load_modules() {
     moderngl = PyImport_ImportModule("moderngl");
     if (!moderngl) {
--- moderngl/mgl/internal/modules.hpp
+++ moderngl/mgl/internal/modules.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "python.hpp"
 
+/* Python modules */
+
 extern PyObject * moderngl;
 
 bool load_modules();
