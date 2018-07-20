cb53ca5fd621a31de75a2af875716e4e931eab59
Szabolcs Dombi <cprogrammer1994@gmail.com>
2018-07-20 17:46:38 +0300

    add unpack_size

--- moderngl/mgl/internal/tools.cpp
+++ moderngl/mgl/internal/tools.cpp
@@ -47,3 +47,60 @@ void assert_slots_len(PyTypeObject * type, int slots_len) {
 
     PyErr_Format(PyExc_Exception, "remaining slots in %s", type->tp_name);
 }
+
+/* Takes an int or str and converts it into a Py_ssize_t.
+ * Examples: 1000 -> 1000, '32KB' -> 32768, '1MB' -> 1048576.
+ */
+Py_ssize_t unpack_size(PyObject * size) {
+	if (PyLong_Check(size)) {
+		return PyLong_AsSsize_t(size);
+	}
+
+	if (!PyUnicode_Check(size)) {
+		PyErr_Format(PyExc_TypeError, "invalid size");
+		return 0;
+	}
+
+	const char * str = PyUnicode_AsUTF8(size);
+
+	char first_chr = *str++;
+	if (first_chr < '1' || first_chr > '9') {
+		PyErr_Format(PyExc_TypeError, "invalid size");
+		return 0;
+	}
+
+	Py_ssize_t value = first_chr - '0';
+
+	while (char chr = *str++) {
+		if (chr < '0' || chr > '9') {
+			switch (chr) {
+				case 'G':
+					value *= 1024;
+
+				case 'M':
+
+					value *= 1024;
+
+				case 'K':
+					value *= 1024;
+					if (*str++ != 'B') {
+						return 0;
+					}
+
+				case 'B':
+					if (*str++) {
+						return 0;
+					}
+
+				case 0:
+					break;
+
+				default:
+					return 0;
+			}
+			break;
+		}
+		value = value * 10 + chr - '0';
+	}
+	return value;
+}
--- moderngl/mgl/internal/tools.hpp
+++ moderngl/mgl/internal/tools.hpp
@@ -14,3 +14,4 @@ T * dup(const T & t) {
 PyTypeObject * detect_class(PyObject * module, const char * name, int & slots_len);
 int slot_offset(PyTypeObject * type, const char * name, int & counter);
 void assert_slots_len(PyTypeObject * type, int slots_len);
+Py_ssize_t unpack_size(PyObject * size);
