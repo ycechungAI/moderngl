da861f2715ef58d8dd966b80880f624d54a2f94c
Szabolcs Dombi <cprogrammer1994@gmail.com>
2018-07-24 17:52:34 +0300

    try numpy assert

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
 
+
 @pytest.mark.usefixtures('ctx')
-def test_adding(ctx):
-    assert ctx.version_code != 0
+def test_hello(ctx):
+    np.testing.assert_almost_equal([1.0, 1.0 + 1e-8], [1.0, 1.0])
