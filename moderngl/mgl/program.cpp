5203dacc26d3b37514d5f8b6dfbe5ce5760bcff1
Szabolcs Dombi <cprogrammer1994@gmail.com>
2018-07-25 08:46:02 +0300

    test

--- moderngl/mgl/program.cpp
+++ moderngl/mgl/program.cpp
@@ -126,7 +126,7 @@ PyObject * MGLContext_meth_program(MGLContext * self, PyObject * const * args, P
 		// PyObject * info = PyUnicode_FromStringAndSize(log_text, log_len);
 		// PyObject_CallFunctionObjArgs(moderngl_linker_error, info, 0);
 		// PyObject_CallFunctionObjArgs(moderngl_linker_error, args[0], args[1], args[2], args[3], args[4], info, 0);
-		PyObject_CallFunctionObjArgs(moderngl_linker_error, args[0], args[1], args[2], args[3], args[4], args[0], 0);
+		PyObject_CallFunctionObjArgs(moderngl_linker_error, args[0], args[1], args[2], args[3], args[4], 0);
         // PyObject * args = PyTuple_Pack(6, args[0], args[1], args[2], args[3], args[4], info);
 		// PyObject_Call(moderngl_linker_error, args, 0);
 		// Py_DECREF(info);
