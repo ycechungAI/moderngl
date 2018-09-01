1dff976aee8bbb62fe88b0788ea0e2cff29634ab
Szabolcs Dombi <cprogrammer1994@gmail.com>
2018-09-01 14:43:28 +0300

    trace

--- tests/context.py
+++ tests/context.py
@@ -1,5 +1,15 @@
+import inspect
+
 import moderngl as mgl
 import pytest
+
+
+@mgl.tracer
+def tracer(name, error):
+    if error:
+        info = inspect.stack()[-2]
+        print(info.function, info.frame.f_locals, name, error)
+
 
 @pytest.fixture(scope='module')
 def ctx():
