eb3687f4c66df39cf36ef0a27e87d3e1b3b09505
Szabolcs Dombi <cprogrammer1994@gmail.com>
2018-09-01 14:48:54 +0300

    errors

--- tests/context.py
+++ tests/context.py
@@ -4,11 +4,20 @@ import moderngl as mgl
 import pytest
 
 
+errors = {
+    0x0000: 'GL_NO_ERROR',
+    0x0500: 'GL_INVALID_ENUM',
+    0x0501: 'GL_INVALID_VALUE',
+    0x0502: 'GL_INVALID_OPERATION',
+}
+
+
 @mgl.tracer
 def tracer(name, error):
-    if error:
-        info = inspect.stack()[-2]
-        print(info.function, info.frame.f_locals, name, error)
+    print(name, errors.get(error, hex(error)))
+    # if error:
+    #     info = inspect.stack()[-2]
+    #     print(info.function, info.frame.f_locals, name, error)
 
 
 @pytest.fixture(scope='module')
