0ea3b733ec79daca9a27586f76d69a8ce9c2db21
Szabolcs Dombi <cprogrammer1994@gmail.com>
2018-09-30 11:25:07 +0300

    fix spaces

--- moderngl/mgl/texture.hpp
+++ moderngl/mgl/texture.hpp
@@ -8,15 +8,15 @@ struct MGLTexture {
     PyObject_HEAD
     PyObject * wrapper;
     MGLContext * context;
-	MGLDataType * data_type;
-	int texture_obj;
-	int texture_target;
-	int width;
-	int height;
-	int depth;
-	int components;
-	int levels;
-	int samples;
+    MGLDataType * data_type;
+    int texture_obj;
+    int texture_target;
+    int width;
+    int height;
+    int depth;
+    int components;
+    int levels;
+    int samples;
 };
 
 PyObject * MGLContext_meth_program(MGLContext * self, PyObject * const * args, Py_ssize_t nargs);
