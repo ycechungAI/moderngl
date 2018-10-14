5ce95825af4873f01151022bd9b31da56a99fbb3
Szabolcs Dombi <cprogrammer1994@gmail.com>
2018-10-14 20:04:55 +0300

    add configuration

---
+++ moderngl/mgl/configuration.cpp
@@ -0,0 +1,28 @@
+#include "configuration.hpp"
+
+#include "internal/modules.hpp"
+
+/* MGLContext.configure(...)
+ */
+PyObject * MGLContext_meth_configure(MGLContext * self, PyObject * const * args, Py_ssize_t nargs) {
+    if (nargs != 1) {
+        // TODO: error
+        return 0;
+    }
+
+    PyObject * configuration = args[0];
+
+    if (PyObject * formatter = PyDict_GetItemString(configuration, "glsl_compiler_error")) {
+        replace_object(self->glsl_compiler_error, formatter);
+    }
+
+    if (PyObject * formatter = PyDict_GetItemString(configuration, "glsl_linker_error")) {
+        replace_object(self->glsl_linker_error, formatter);
+    }
+
+    if (PyObject * gc = PyDict_GetItemString(configuration, "gc")) {
+        replace_object(self->gc, gc);
+    }
+
+    Py_RETURN_NONE;
+}
---
+++ moderngl/mgl/configuration.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include "mgl.hpp"
+#include "context.hpp"
+
+PyObject * MGLContext_meth_configure(MGLContext * self, PyObject * const * args, Py_ssize_t nargs);
