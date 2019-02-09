fe3631afa2bfe0b76d37cd6068091e62bb67aea8
usernameak <sitemakers.3dn.ru@gmail.com>
2019-02-09 14:56:55 +0200

    GLES -> GLESv3

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ libraries = {
     'linux': ['GL', 'dl', 'X11'],
     'cygwin': ['GL', 'X11'],
     'darwin': [],
-    'android': ['GLES', 'EGL'],
+    'android': ['GLESv3', 'EGL'],
 }
 
 extra_compile_args = {
