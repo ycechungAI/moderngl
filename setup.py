8a3e52a5edaeeb1cd490051644360cfcc7f411a8
Szabolcs Dombi <cprogrammer1994@gmail.com>
2019-08-03 14:27:48 +0300

    restore linux and darwin platforms

--- setup.py
+++ setup.py
@@ -1,8 +1,61 @@
+import platform
+import sys
+
 from setuptools import Extension, setup
 
-# tools = ','.join(str(x) for x in open('moderngl_tools.py', 'rb').read())
-# tools = f'const char moderngl_tools[] = {{{tools}}};\n'
-# open('moderngl_tools.hpp', 'w').write(tools)
+if sys.version_info < (3, 0):
+    raise Exception('Python 2 is not supported!')
+
+PLATFORMS = {'windows', 'linux', 'darwin', 'cygwin'}
+
+target = platform.system().lower()
+
+for known in PLATFORMS:
+    if target.startswith(known):
+        target = known
+
+if target not in PLATFORMS:
+    target = 'linux'
+
+if target == 'darwin':
+    import os
+    from distutils.sysconfig import get_config_var
+    from distutils.version import LooseVersion
+    if 'MACOSX_DEPLOYMENT_TARGET' not in os.environ:
+        current_system = LooseVersion(platform.mac_ver()[0])
+        python_target = LooseVersion(get_config_var('MACOSX_DEPLOYMENT_TARGET'))
+        if python_target < '10.9' and current_system >= '10.9':
+            os.environ['MACOSX_DEPLOYMENT_TARGET'] = '10.9'
+
+libraries = {
+    'windows': ['gdi32', 'opengl32', 'user32'],
+    'linux': ['GL', 'dl', 'X11'],
+    'cygwin': ['GL', 'X11'],
+    'darwin': [],
+}
+
+extra_compile_args = {
+    'windows': [],
+    'linux': ['-fpermissive'],
+    'cygwin': [],
+    'darwin': ['-Wno-deprecated-declarations'],
+}
+
+extra_linker_args = {
+    'windows': [],
+    'linux': [],
+    'cygwin': [],
+    'darwin': ['-framework', 'OpenGL', '-Wno-deprecated'],
+}
+
+context_source = {
+    'windows': ['moderngl/gl_context_windows.cpp'],
+    'linux': ['moderngl/gl_context_linux.cpp'],
+    'cygwin': ['moderngl/gl_context_linux.cpp'],
+    'darwin': ['moderngl/gl_context_darwin.cpp'],
+}
+
+short_description = 'ModernGL: High performance rendering for Python 3'
 
 ext = Extension(
     name='moderngl',
@@ -10,10 +63,21 @@ ext = Extension(
         'moderngl/moderngl.cpp',
         'moderngl/extras.cpp',
         'moderngl/gl_methods.cpp',
-        'moderngl/gl_context_windows.cpp',
+    ] + context_source[target],
+    libraries=libraries[target],
+    extra_compile_args=extra_compile_args[target],
+    extra_link_args=extra_linker_args[target],
+    depends=[
+        'moderngl/common.hpp',
+        'moderngl/data_types.hpp',
+        'moderngl/extras.hpp',
+        'moderngl/gl_context.hpp',
+        'moderngl/gl_methods.hpp',
+        'moderngl/moderngl.hpp',
+        'moderngl/opengl.hpp',
+        'moderngl/tools.hpp',
+        'setup.py',
     ],
-    extra_compile_args=['-fpermissive'],
-    libraries=['opengl32', 'gdi32', 'user32'],
 )
 
 setup(
