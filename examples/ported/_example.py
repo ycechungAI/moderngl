d0917cdf3865a2288999a3bed322390e01960027
Einar Forselv <eforselv@gmail.com>
2020-12-13 04:57:08 +0100

    Disable msaa by default for examples

--- examples/ported/_example.py
+++ examples/ported/_example.py
@@ -9,7 +9,6 @@ class Example(mglw.WindowConfig):
     window_size = (1280, 720)
     aspect_ratio = 16 / 9
     resizable = True
-    samples = 4
 
     resource_dir = os.path.normpath(os.path.join(__file__, '../../data'))
 
