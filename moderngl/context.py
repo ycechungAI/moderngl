3074d21a372efc77edcb3fbb726dbf9deb8c039d
Einar Forselv <eforselv@gmail.com>
2021-02-13 05:44:51 +0100

    Texture filter docstrings

--- moderngl/context.py
+++ moderngl/context.py
@@ -166,6 +166,38 @@ class Context:
     #: on the TES's settings. 
     PATCHES = 0x000E
 
+    # Texture filters
+    #: Returns the value of the texture element that is nearest 
+    #: (in Manhattan distance) to the specified texture coordinates. 
+    NEAREST = 0x2600
+    #: Returns the weighted average of the four texture elements
+    #: that are closest to the specified texture coordinates.
+    #: These can include items wrapped or repeated from other parts
+    #: of a texture, depending on the values of texture repeat mode,
+    #: and on the exact mapping. 
+    LINEAR = 0x2601
+    #: Chooses the mipmap that most closely matches the size of the
+    #: pixel being textured and uses the ``NEAREST`` criterion (the texture
+    #: element closest to the specified texture coordinates) to produce
+    #: a texture value. 
+    NEAREST_MIPMAP_NEAREST = 0x2700
+    #: Chooses the mipmap that most closely matches the size of the pixel
+    #: being textured and uses the ``LINEAR`` criterion (a weighted average
+    #: of the four texture elements that are closest to the specified
+    #: texture coordinates) to produce a texture value. 
+    LINEAR_MIPMAP_NEAREST = 0x2701
+    #: Chooses the two mipmaps that most closely match the size of the
+    #: pixel being textured and uses the ``NEAREST`` criterion (the texture
+    #: element closest to the specified texture coordinates ) to produce
+    #: a texture value from each mipmap. The final texture value is a
+    #: weighted average of those two values.
+    NEAREST_MIPMAP_LINEAR = 0x2702
+    #: Chooses the two mipmaps that most closely match the size of the pixel
+    #: being textured and uses the ``LINEAR`` criterion (a weighted average
+    #: of the texture elements that are closest to the specified texture
+    #: coordinates) to produce a texture value from each mipmap.
+    #: The final texture value is a weighted average of those two values.
+    LINEAR_MIPMAP_LINEAR = 0x2703    
 
     # Blend functions
     #: (0,0,0,0)
