3f472a9fcefb1b2ac62f29df91a27ebb3801e8b6
Einar Forselv <eforselv@gmail.com>
2021-02-19 00:59:10 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -154,7 +154,7 @@ classifiers = [
 
 setup(
     name='moderngl',
-    version='5.6.3',
+    version='5.6.4',
     description=short_description,
     long_description=long_description,
     long_description_content_type='text/markdown',
