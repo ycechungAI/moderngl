ba40b67bc993ae6aa3fb02d3fca67553a3c058a1
Einar Forselv <eforselv@gmail.com>
2021-03-07 02:36:20 +0100

    game of life example comments

--- examples/conways_game_of_life.py
+++ examples/conways_game_of_life.py
@@ -1,3 +1,17 @@
+"""
+A Game of Life implementation using transform feedback.
+
+We calculate the next state of the map with transform()
+meaning a vertex shader will geneate the new state into a buffer.
+This buffer is then written into the texture we display.
+This is a fast vram to vram copy.
+
+Comments:
+
+Another way to do this is simply rendering to Framebuffers.
+We could also drop the grid and modulo gl_VertexID tranforming
+with an empty vertex array.
+"""
 import numpy as np
 
 import moderngl
@@ -13,8 +27,11 @@ class Conway(Example):
         super().__init__(**kwargs)
 
         width, height = self.window_size
+        # vertex data for rendering the texture
         canvas = np.array([0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0]).astype('f4')
+        # Initial state of the map (random)
         pixels = np.round(np.random.rand(width, height)).astype('f4')
+        # Generate a buffer with all the pixel positions in the map
         grid = np.dstack(np.mgrid[0:height, 0:width][::-1]).astype('i4')
 
         self.prog = self.ctx.program(
@@ -89,22 +106,27 @@ class Conway(Example):
 
         self.texture = self.ctx.texture((width, height), 1, pixels.tobytes(), dtype='f4')
         self.texture.filter = (moderngl.NEAREST, moderngl.NEAREST)
-        self.texture.swizzle = 'RRR1'
-        self.texture.use()
+        self.texture.swizzle = 'RRR1'  # What components texelFetch will get from the texture (in shader)
 
+        # A quad covering the screen (scaled in vertex shader)
         self.vbo = self.ctx.buffer(canvas)
         self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'in_vert')
 
+        # Transform vertex array to generate new map state
         self.text = self.ctx.buffer(grid)
         self.tao = self.ctx.simple_vertex_array(self.transform, self.text, 'in_text')
-        self.pbo = self.ctx.buffer(reserve=pixels.nbytes)
+        self.pbo = self.ctx.buffer(reserve=pixels.nbytes)  # buffer to store the result
 
     def render(self, time, frame_time):
         self.ctx.clear(1.0, 1.0, 1.0)
+        self.texture.use()
 
+        # Generate the new map and write that to the pbo buffer
         self.tao.transform(self.pbo)
+        # Copy the pbo into the texture
         self.texture.write(self.pbo)
 
+        # Render the texture
         self.vao.render(moderngl.TRIANGLE_STRIP)
 
 
