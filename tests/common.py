6e58879db5814a29606a96f1dd3947d597840e5a
einarf <eforselv@gmail.com>
2021-03-07 05:13:57 +0100

    Tests: Always clear fbo

--- tests/common.py
+++ tests/common.py
@@ -37,6 +37,7 @@ def get_context(require=330) -> moderngl.Context:
             return None
 
     entry['fbo'].use()
+    entry['fbo'].clear()
     entry['ctx'].__enter__()
     return entry['ctx']
  
