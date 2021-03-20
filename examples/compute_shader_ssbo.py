168bb957cda7fdd0ef5845a9453d0421d896a49d
Einar Forselv <eforselv@gmail.com>
2021-03-20 15:32:52 +0100

    Example: Improve/simplify compute_shader_ssbo

--- examples/compute_shader_ssbo.py
+++ examples/compute_shader_ssbo.py
@@ -1,66 +1,104 @@
 
 '''
-    example of using shader storage buffer in compute shader.
+Example of using shader storage buffer in compute shader.
+We read from a buffer and write the result to another buffer.
+Every frame we swap the buffers around transforming positions
+of balls.
 
-    requirements:
-     - numpy
-     - PyQt5
+Buffer.bind_to_storage_buffer is used to bind a buffer as storage buffer
+to a specific binding point specified in the compute program.
 
-    author: minu jeong
+In addition we render the balls using a geometry shader to easily
+batch draw them all in one render call.
+
+author: minu jeong
+modified by: einarf
 '''
-
-
 import math
 import random
-
 import numpy as np
-import moderngl as mg
-from PyQt5 import QtWidgets
-from PyQt5 import QtCore
+from ported._example import Example
 
 
-simple_vertex_shader_code = """
+items_vertex_shader_code = """
     #version 430
 
-    in vec2 in_vert;
-    in vec2 in_uv;
-    in vec3 in_col;
+    in vec4 in_vert;
+    in vec4 in_col;
 
-    out vec2 v_pos;
-    out vec2 v_uv;
-    out vec3 v_col;
+    out vec3 v_color;
 
-    out float v_rad;
     void main()
     {
-        v_uv = in_uv;
-        v_col = in_col;
-
-        gl_Position = vec4(in_vert.xy, 0.0, 1.0);
-        v_pos = abs(in_vert.xy);
+        gl_Position = vec4(in_vert.xyzw); // x, y, 0, radius
+        v_color = in_col.xyz;
     }
     """
 
-simple_fragment_shader_code = """
+# Geometry shader turning the points into triangle strips.
+# This can also be done with point sprites.
+items_geo_shader = """
+    #version 330
+
+    layout(points) in;
+    layout(triangle_strip, max_vertices=4) out;
+
+    in vec3 v_color[];
+    out vec2 uv;
+    out vec3 color;
+
+    void main() {
+        float radius = gl_in[0].gl_Position.w;
+        vec2 pos = gl_in[0].gl_Position.xy;
+
+        // Emit the triangle strip creating a "quad"
+        // Lower left
+        gl_Position = vec4(pos + vec2(-radius, -radius), 0, 1);
+        color = v_color[0];
+        uv = vec2(0, 0);
+        EmitVertex();
+
+        // upper left
+        gl_Position = vec4(pos + vec2(-radius, radius), 0, 1);
+        color = v_color[0];
+        uv = vec2(0, 1);
+        EmitVertex();
+
+        // lower right
+        gl_Position = vec4(pos + vec2(radius, -radius), 0, 1);
+        color = v_color[0];
+        uv = vec2(1, 0);
+        EmitVertex();
+
+        // upper right
+        gl_Position = vec4(pos + vec2(radius, radius), 0, 1);
+        color = v_color[0];
+        uv = vec2(1, 1);
+        EmitVertex();
+
+        EndPrimitive();
+    }
+"""
+
+items_fragment_shader_code = """
     #version 430
 
-    in vec2 v_pos;
-    in vec2 v_uv;
-    in vec3 v_col;
+    in vec2 uv;
+    in vec3 color;
     out vec4 out_color;
     void main()
     {
-        if (length(vec2(0.5, 0.5) - v_uv.xy) > 0.25)
+        if (length(vec2(0.5, 0.5) - uv.xy) > 0.25)
         {
             discard;
         }
 
-        vec3 c = v_col;
-        c.xy += v_uv.xy * 0.05;
-        c.xy += v_pos.xy * 0.75;
+        vec3 c = color;
+        c.xy += uv.xy * 0.05;
+        // c.xy += v_pos.xy * 0.75;
         out_color = vec4(c, 1.0);
     }
-    """
+"""
 
 # calc position with compute shader
 compute_worker_shader_code = """
@@ -69,11 +107,12 @@ compute_worker_shader_code = """
 
 layout(local_size_x=GROUP_SIZE) in;
 
+// All values are vec4s because of block alignment rules (keep it simple)
 struct Ball
 {
-    vec4 pos;
-    vec4 vel;
-    vec4 col;
+    vec4 pos; // x, y, 0, radius
+    vec4 vel; // x, y (velocity)
+    vec4 col; // r, g, b (color)
 };
 
 layout(std430, binding=0) buffer balls_in
@@ -131,75 +170,32 @@ void main()
 }
 """
 
+class ComputeShaderSSBO(Example):
+    title = "Compute Shader SSBO"
+    gl_version = 4, 3
+    window_size = 600, 600
+    aspect_ratio = 1.0
 
-class ComputeToggleTimer(QtCore.QThread):
-    compute_update_signal = QtCore.pyqtSignal(bool)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.COUNT = 256  # number of balls
+        self.STRUCT_SIZE = 12  # number of floats
 
-    def __init__(self):
-        super(ComputeToggleTimer, self).__init__()
-        self.toggle_buffer = False
-
-    def run(self):
-        while True:
-            self.compute_update_signal.emit(self.toggle_buffer)
-            self.toggle_buffer = not self.toggle_buffer
-            self.msleep(32)
-
-
-class Renderer(QtWidgets.QOpenGLWidget):
-    COUNT = 256
-    STRUCT_SIZE = 12
-
-    worker_thread = None
-
-    is_running_computeshader = False
-
-    def __init__(self):
-        super(Renderer, self).__init__()
-        W, H = 600, 600
-        self.setMinimumSize(W, H)
-        self.setMaximumSize(W, H)
-        self.viewport = (0, 0, W, H)
-
-    def update_computeworker(self, toggle):
-        if toggle:
-            a, b = 0, 1
-            self.target_buffer = self.compute_buffer_b
-        else:
-            a, b = 1, 0
-            self.target_buffer = self.compute_buffer_a
-
-        self.compute_buffer_a.bind_to_storage_buffer(a)
-        self.compute_buffer_b.bind_to_storage_buffer(b)
-        self.compute_shader.run(group_x=Renderer.STRUCT_SIZE)
-
-    def initializeGL(self):
-        self.context = mg.create_context()
-        self.program = self.context.program(
-            vertex_shader=simple_vertex_shader_code,
-            fragment_shader=simple_fragment_shader_code
+        # Program for drawing the balls / items
+        self.program = self.ctx.program(
+            vertex_shader=items_vertex_shader_code,
+            geometry_shader=items_geo_shader,
+            fragment_shader=items_fragment_shader_code
         )
 
-        index_data = np.array([
-            0, 1, 2,
-            1, 2, 3,
-        ]).astype('i4')
-        self.index_buffer = self.context.buffer(index_data)
+        # Load compute shader
+        compute_shader_code_parsed = compute_worker_shader_code.replace("%COMPUTE_SIZE%", str(self.COUNT))
+        self.compute_shader = self.ctx.compute_shader(compute_shader_code_parsed)
 
-        self.vbo_data = np.array([
-            # x     y     u    v     r    g    b
-            [-1.0, -1.0,  0.0, 0.0,  0.0, 0.0, 0.0],
-            [-1.0, +1.0,  0.0, 1.0,  0.0, 0.0, 0.0],
-            [+1.0, -1.0,  1.0, 0.0,  0.0, 0.0, 0.0],
-            [+1.0, +1.0,  1.0, 1.0,  0.0, 0.0, 0.0],
-        ]).astype('f4')
-
-        compute_shader_code_parsed = compute_worker_shader_code.replace("%COMPUTE_SIZE%", str(Renderer.COUNT))
-        self.compute_shader = self.context.compute_shader(compute_shader_code_parsed)
-
+        # Generate initial data
         compute_data = []
-        for i in range(Renderer.COUNT):
-            _angle = (i / Renderer.COUNT) * math.pi * 2.0
+        for i in range(self.COUNT):
+            _angle = (i / self.COUNT) * math.pi * 2.0
             _dist = 0.125
             radius = random.random() * 0.02 + 0.02
             x = math.cos(_angle) * _dist
@@ -216,73 +212,36 @@ class Renderer(QtWidgets.QOpenGLWidget):
             b = 1.0 * random.random()
             a = 1.0
 
-            # match bytes
             compute_data.append(
                 [[x, y, z, w], [vx, vy, vz, vw], [r, g, b, a]])
 
-        compute_data = np.array(compute_data).astype('f4')
-        compute_data_bytes = compute_data.tobytes()
-
-        self.compute_buffer_a = self.context.buffer(compute_data_bytes)
-        self.compute_buffer_b = self.context.buffer(compute_data_bytes)
+        compute_data = np.array(compute_data, dtype="f4")
+        self.compute_buffer_a = self.ctx.buffer(compute_data)
+        self.compute_buffer_b = self.ctx.buffer(compute_data)
         self.target_buffer = self.compute_buffer_b
 
-        self.timer_thread = ComputeToggleTimer()
-        self.timer_thread.compute_update_signal.connect(self.update_computeworker)
-        self.timer_thread.start()
-
-        self.item_vertex_buffer = self.context.buffer(reserve=112)
-        self.vertex_array = self.context.vertex_array(
-            self.program,
-            [(
-                self.item_vertex_buffer,
-                '2f 2f 3f',
-                'in_vert', 'in_uv', 'in_col'
-            )],
-            self.index_buffer,
+        # Prepare vertex arrays to drawing balls using the compute shader buffers are input
+        # We use 4x4 (padding format) to skip the velocity date (not needed for drawing the balls)
+        self.balls_a = self.ctx.vertex_array(
+            self.program, [(self.compute_buffer_a, '4f 4x4 4f', 'in_vert', 'in_col')],
+        )
+        self.balls_b = self.ctx.vertex_array(
+            self.program, [(self.compute_buffer_b, '4f 4x4 4f', 'in_vert', 'in_col')],
         )
 
-    def paintGL(self):
-        self.context.viewport = self.viewport
-        data = np.frombuffer(self.target_buffer.read(), dtype='f4')
-        data = data.reshape((Renderer.COUNT, Renderer.STRUCT_SIZE))
+    def render(self, time, frame_time):
+        # Calculate the next position of the balls with compute shader
+        self.compute_buffer_a.bind_to_storage_buffer(0)
+        self.compute_buffer_b.bind_to_storage_buffer(1)
+        self.compute_shader.run(group_x=self.STRUCT_SIZE)
 
-        self.vaos = []
-        for item in data:
-            # popout 1x1 quad
-            item_vertex = self.vbo_data.copy()
+        # Batch draw the balls
+        self.balls_b.render(mode=self.ctx.POINTS)
 
-            # apply rad
-            item_vertex[:, [0, 1]] *= item[3]
-
-            # xy
-            item_vertex[:, 0] += item[0]
-            item_vertex[:, 1] += item[1]
-
-            # rgb
-            item_vertex[:, 4] = item[8]
-            item_vertex[:, 5] = item[9]
-            item_vertex[:, 6] = item[10]
-
-            # generate vertex buffer object to render
-            self.item_vertex_buffer.write(item_vertex.tobytes())
-            self.vertex_array.render()
-
-        self.update()
-
-
-def main():
-    app = QtWidgets.QApplication([])
-    mainwin = QtWidgets.QMainWindow()
-    root = QtWidgets.QFrame()
-    root_ly = QtWidgets.QVBoxLayout()
-    renderer = Renderer()
-    root_ly.addWidget(renderer)
-    root.setLayout(root_ly)
-    mainwin.setCentralWidget(root)
-    mainwin.show()
-    app.exec()
+        # Swap the buffers and vertex arrays around for next frame
+        self.compute_buffer_a, self.compute_buffer_b = self.compute_buffer_b, self.compute_buffer_a
+        self.balls_a, self.balls_b = self.balls_b, self.balls_a
 
 
 if __name__ == "__main__":
-    main()
+    ComputeShaderSSBO.run()
