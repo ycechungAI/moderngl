bd12e4184e2f238d0f9ac1352705a574af743bb6
Einar Forselv <eforselv@gmail.com>
2022-07-19 20:16:06 +0200

    Test: Simplify doc tests (no longer test signatures)

--- tests/test_documentation.py
+++ tests/test_documentation.py
@@ -9,30 +9,22 @@ import moderngl
 
 class TestCase(unittest.TestCase):
     root = os.path.dirname(os.path.dirname(__file__))
-    maxDiff = 10000
+    maxDiff = 10_000
 
     def validate_cls(self, filename, classname, ignore, include=None):
         docs = self._read_rst_file(filename)
 
-        methods = re.findall(r'^\.\. automethod:: ([^\(\n]+)([^\n]+)', docs, flags=re.M)
+        methods = re.findall(r'^\.\. automethod:: ([^\(\n]+)', docs, flags=re.M)
         attributes = re.findall(r'^\.\. autoattribute:: ([^\n]+)', docs, flags=re.M)
-        documented = set(filter(lambda x: x.startswith(classname), [a for a, b in methods] + attributes))
+
+        documented = set(filter(lambda x: x.startswith(classname), methods + attributes))
         implemented = set(classname + '.' + x for x in dir(getattr(moderngl, classname)) if not x.startswith('_'))
+
         include = set('{}.{}'.format(classname, e) for e in include or [])
         ignored = set(classname + '.' + x for x in ignore)
+
         self.assertSetEqual(implemented - documented - ignored, set(), msg='Implemented but not Documented')
         self.assertSetEqual(documented - implemented - include, set(), msg='Documented but not Implemented')
-
-        attribute_access = re.compile(r"[a-zA-Z]\w*\.(\w+)")
-        for method, docsig in methods:
-            classname, methodname = method.split('.')
-            sig = str(inspect.signature(getattr(getattr(moderngl, classname), methodname)))
-            sig = sig.replace('self, ', '').replace(' -> None', '')
-            for _ in range(2):
-                sig = re.sub(attribute_access, r'\1', sig)
-            sig = sig.replace('(self)', '()').replace(', *,', ',').replace('(*, ', '(')
-            sig = re.sub(r'-> \'(\w+)\'', r'-> \1', sig)
-            self.assertEqual(docsig, sig, msg=filename + '::' + method)
 
     def validate_module(self, filename, ignore=[], include=None):
         docs = self._read_rst_file(filename)
@@ -45,8 +37,6 @@ class TestCase(unittest.TestCase):
         include = set()
         self.assertSetEqual(implemented - documented - ignored, set(), msg='Implemented but not Documented')
         self.assertSetEqual(documented - implemented - include, set(), msg='Documented but not Implemented')
-
-        print(implemented)
 
     def _read_rst_file(self, filename):
         with open(os.path.normpath(os.path.join(self.root, 'docs', 'reference', filename))) as f:
