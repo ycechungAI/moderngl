316226e21abccc3452ffe8848a047300c523435e
Einar Forselv <eforselv@gmail.com>
2022-10-28 06:29:04 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ project_urls = {
 
 setup(
     name='moderngl',
-    version='5.7.0',
+    version='5.7.1',
     description=short_description,
     long_description=long_description,
     long_description_content_type='text/markdown',
