a29e33e2288cfbb4cdd09efa1c4440537cd2aaaf
Einar Forselv <eforselv@gmail.com>
2022-12-15 19:13:52 +0100

    Add missing texture array testing

--- tests/test_texture_array.py
+++ tests/test_texture_array.py
@@ -18,10 +18,32 @@ def test_create(ctx):
     assert texture.repeat_y is True
     assert texture.filter == (moderngl.LINEAR, moderngl.LINEAR)
     assert texture.swizzle == "RGBA"
-    assert texture.anisotropy == 1.0
+    assert hash(texture) == id(texture)
+    assert texture == texture
 
+    texture.repeat_x = False
+    texture.repeat_y = False
+    assert texture.repeat_x is False
+    assert texture.repeat_y is False
+
+    assert texture.swizzle == "RGBA"
+    texture.swizzle = "RGBA"
+    assert texture.swizzle == "RGBA"
+
+    assert texture.anisotropy == 1.0
+    texture.anisotropy = ctx.max_anisotropy
+    assert texture.anisotropy == ctx.max_anisotropy
+
+    assert texture.filter == (moderngl.LINEAR, moderngl.LINEAR)
+    texture.filter = moderngl.NEAREST, moderngl.NEAREST
+    assert texture.filter == (moderngl.NEAREST, moderngl.NEAREST)
     texture.build_mipmaps()
     assert texture.filter == (moderngl.LINEAR_MIPMAP_LINEAR, moderngl.LINEAR)
+
+
+def test_repr(ctx):
+    texture = ctx.texture_array((64, 32, 8), 4)
+    assert repr(texture) == "<TextureArray: {}>".format(texture.glo)
 
 
 def test_texture_default_filter(ctx):
