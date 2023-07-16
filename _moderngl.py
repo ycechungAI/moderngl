cee293633750432d53725f9ba936e0a8a3c2b559
compik710 <68602173+compik710@users.noreply.github.com>
2023-07-17 00:11:21 +0900

    Enhanced typing, parser optimization in _moderngl.py

--- _moderngl.py
+++ _moderngl.py
@@ -1,5 +1,5 @@
 import struct
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, List
 
 
 class Attribute:
@@ -366,64 +366,193 @@ def make_varying(name, number, array_length, dimension):
     return res
 
 
-def parse_spv_inputs(raw_spv: bytes):
-    spv = struct.unpack(str(len(raw_spv)//4)+'I', raw_spv)
+class Spv:
+    INT32 = 1 << 0
+    UINT32 = 1 << 1
+    FLOAT32 = 1 << 2
+    FLOAT64 = 1 << 3
 
-    assert spv[0] == 0x07230203
+    VEC2 = 1 << 10
+    VEC3 = 1 << 11
+    VEC4 = 1 << 12
+
+    MAT2 = 1 << 20
+    MAT3 = 1 << 21
+    MAT4 = 1 << 22
+
+    UNKNOWN = 1 << 30
+
+
+TRANSLATION_TABLE_SPIRV_GLSL = {  # spv_type : gl_type
+    Spv.INT32: 0x1404,  # GL_INT
+    Spv.INT32 | Spv.VEC2: 0x8b53,  # GL_INT_VEC2
+    Spv.INT32 | Spv.VEC3: 0x8b54,  # GL_INT_VEC3
+    Spv.INT32 | Spv.VEC4: 0x8b55,  # GL_INT_VEC4
+    Spv.UINT32: 0x1405,  # GL_UNSIGNED_INT
+    Spv.UINT32 | Spv.VEC2: 0x8dc6,  # GL_UNSIGNED_INT_VEC2
+    Spv.UINT32 | Spv.VEC3: 0x8dc7,  # GL_UNSIGNED_INT_VEC3
+    Spv.UINT32 | Spv.VEC4: 0x8dc8,  # GL_UNSIGNED_INT_VEC4
+    Spv.FLOAT32: 0x1406,  # GL_FLOAT
+    Spv.FLOAT32 | Spv.VEC2: 0x8b50,  # GL_FLOAT_VEC2
+    Spv.FLOAT32 | Spv.VEC3: 0x8b51,  # GL_FLOAT_VEC3
+    Spv.FLOAT32 | Spv.VEC4: 0x8b52,  # GL_FLOAT_VEC4
+    Spv.FLOAT64: 0x140a,  # GL_DOUBLE
+    Spv.FLOAT64 | Spv.VEC2: 0x8ffc,  # GL_DOUBLE_VEC2
+    Spv.FLOAT64 | Spv.VEC3: 0x8ffd,  # GL_DOUBLE_VEC3
+    Spv.FLOAT64 | Spv.VEC4: 0x8ffe,  # GL_DOUBLE_VEC4
+    Spv.FLOAT32 | Spv.VEC2 | Spv.MAT2: 0x8b5a,  # GL_FLOAT_MAT2
+    Spv.FLOAT32 | Spv.VEC3 | Spv.MAT2: 0x8b65,  # GL_FLOAT_MAT2x3
+    Spv.FLOAT32 | Spv.VEC4 | Spv.MAT2: 0x8b66,  # GL_FLOAT_MAT2x4
+    Spv.FLOAT32 | Spv.VEC2 | Spv.MAT3: 0x8b67,  # GL_FLOAT_MAT3x2
+    Spv.FLOAT32 | Spv.VEC3 | Spv.MAT3: 0x8b5b,  # GL_FLOAT_MAT3
+    Spv.FLOAT32 | Spv.VEC4 | Spv.MAT3: 0x8b68,  # GL_FLOAT_MAT3x4
+    Spv.FLOAT32 | Spv.VEC2 | Spv.MAT4: 0x8b69,  # GL_FLOAT_MAT4x2
+    Spv.FLOAT32 | Spv.VEC3 | Spv.MAT4: 0x8b6a,  # GL_FLOAT_MAT4x3
+    Spv.FLOAT32 | Spv.VEC4 | Spv.MAT4: 0x8b5c,  # GL_FLOAT_MAT4
+    Spv.FLOAT64 | Spv.VEC2 | Spv.MAT2: 0x8f46,  # GL_DOUBLE_MAT2
+    Spv.FLOAT64 | Spv.VEC3 | Spv.MAT2: 0x8f49,  # GL_DOUBLE_MAT2x3
+    Spv.FLOAT64 | Spv.VEC4 | Spv.MAT2: 0x8f4a,  # GL_DOUBLE_MAT2x4
+    Spv.FLOAT64 | Spv.VEC3 | Spv.MAT2: 0x8f4b,  # GL_DOUBLE_MAT3x2
+    Spv.FLOAT64 | Spv.VEC3 | Spv.MAT3: 0x8f47,  # GL_DOUBLE_MAT3
+    Spv.FLOAT64 | Spv.VEC4 | Spv.MAT3: 0x8f4c,  # GL_DOUBLE_MAT3x4
+    Spv.FLOAT64 | Spv.VEC2 | Spv.MAT4: 0x8f4d,  # GL_DOUBLE_MAT4x2
+    Spv.FLOAT64 | Spv.VEC3 | Spv.MAT4: 0x8f4e,  # GL_DOUBLE_MAT4x3
+    Spv.FLOAT64 | Spv.VEC4 | Spv.MAT4: 0x8f48,  # GL_DOUBLE_MAT4
+}
+
+
+def parse_spv_inputs(spv: bytes):
+    token = lambda i: struct.unpack('I', spv[i * 4 : i * 4 + 4])[0]
+    # spv = struct.unpack(str(len(raw)//4)+'I', spv)
+
+    assert token(0) == 0x07230203
+    """
+    print('version', hex(token(1)))
+    print('generator', hex(token(2)))
+    print('bound', token(3))
+    """
+    
     idx = 5
 
     # == Get max information == #
-    extracted_names = {}  # id : name
-    extracted_storage_class_id = {}  # id : storage_class_id
+    extracted_names: Dict[int, str] = {}  # id : name
+    extracted_storage_class_id: Dict[int, int] = {}  # id : storage_class_id
 
-    pointer_variable = {}  # id : pointer_type_id 
-    pointer_allowed_types = {}  # pointer_type_id : type_id
-    allowed_types = {}  # type_id : [py_type, type_id]
+    pointer_variable: Dict[int, int] = {}  # id : pointer_type_id 
+    pointer_allowed_types: Dict[int, int] = {}  # pointer_type_id : type_id
+    allowed_types: Dict[int, List[int, int]] = {}  # type_id : [spv_type, type_id]
 
-    extracted_types = {}  # id : py_type
-    extracted_location = {}  # id : location
-    while idx < len(spv):
-        args, opcode = spv[idx] >> 16, spv[idx] & 0xffff
+    extracted_types: Dict[int, int] = {}  # id : spv_type
+    extracted_location: Dict[int, int] = {}  # id : location
+    while idx < len(spv)//4:
+        args, opcode = token(idx) >> 16, token(idx) & 0xffff
         if opcode == 5: # OpName
             # We can extract the name of some ids
-            extracted_names[spv[idx + 1]] = raw_spv[idx*4 + 2*4 : idx*4 + args*4].decode()
+            extracted_names[token(idx + 1)] = spv[(idx+2) * 4 : idx * 4 + args*4].decode()
         
         if opcode == 59: # OpVariable
             # We can extract if it is a vertex shader input or not
-            extracted_storage_class_id[spv[idx + 2]] = spv[idx + 3]
+            to_write = token(idx + 3)
+            
+            # There are too many of them to correctly identify in this section of code.
+            # Therefore, for now, I leave only their identifiers.
+            # https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html#Storage_Class
+            #if to_write == 0:
+            #    to_write = 'uniform'
+            #elif to_write == 1:
+            #    to_write = 'input'
+            #elif to_write == 2:
+            #    to_write = 'uniform'
+            #elif to_write == 3:
+            #    to_write = 'output'
+            #else:
+            #    to_write = 'error'
+            
+            extracted_storage_class_id[token(idx + 2)] = to_write
             
             # Mapping pointer_type_id to id of user variables
-            pointer_variable[spv[idx + 2]] = spv[idx + 1]
+            pointer_variable[token(idx + 2)] = token(idx + 1)
         
         if opcode == 71: # OpDecorate
             # We can extract the location here
-            extracted_location[spv[idx + 1]] = spv[idx + 3]
+            extracted_location[token(idx + 1)] = token(idx + 3)
         
         if opcode == 32:  # OpTypePointer
             # Retrieving used (allowed) type_ids for pointer_type_ids
-            pointer_allowed_types[spv[idx + 1]] = spv[idx + 3]
+            pointer_allowed_types[token(idx + 1)] = token(idx + 3)
             # --------------------^pointer_id^^----^^^type_id^^==
-            #print(spv[idx + 1], spv[idx + 2], spv[idx + 3])
+            #print(token(idx + 1), token(idx + 2), token(idx + 3))
         
-        if opcode == 19:  # OpTypeVoid
-            allowed_types[spv[idx + 1]] = ['void', -1]
-        if opcode == 20:  # OpTypeBool
-            allowed_types[spv[idx + 1]] = ['bool', -1]
+        # if opcode == 19:  # OpTypeVoid
+        #     allowed_types[token(idx + 1)] = ['void', -1]
+        # if opcode == 20:  # OpTypeBool
+        #     allowed_types[token(idx + 1)] = ['bool', -1]
          
         if opcode == 21:  # OpTypeInt
-            addware_unsigned = ''
-            if spv[idx + 3] == 1:
-                 addware_unsigned = 'u'
-            allowed_types[spv[idx + 1]] = [f'{addware_unsigned}i{spv[idx + 2]//8}', -1]
+            unsg, bsz = token(idx + 3), token(idx + 2)//8
+            to_write = Spv.UNKNOWN
+            if unsg == 1:
+                if bsz == 4:
+                    to_write = Spv.UINT32
+                elif bsz == 8:
+                    to_write = Spv.UINT64
+            else:
+                if bsz == 4:
+                    to_write = Spv.INT32
+                elif bsz == 8:
+                    to_write = Spv.INT64
+            allowed_types[token(idx + 1)] = [to_write, -1]
             
         if opcode == 22:  # OpTypeFloat
-            allowed_types[spv[idx + 1]] = [f'f{spv[idx + 2]//8}', -1]
+            to_write = Spv.UNKNOWN
+            bsz = token(idx + 2)//8
+            if bsz == 4:
+                to_write = Spv.FLOAT32
+            elif bsz == 8:
+                to_write = Spv.FLOAT64
+            allowed_types[token(idx + 1)] = [to_write, -1]
             
         if opcode == 23:  # OpTypeVector
-            allowed_types[spv[idx + 1]] = [f'vec{spv[idx + 3]}', spv[idx + 2]]
+            to_write = Spv.UNKNOWN
+            vsz = token(idx + 3)
+            if vsz == 2:
+                to_write = Spv.VEC2
+            elif vsz == 3:
+                to_write = Spv.VEC3
+            elif vsz == 4:
+                to_write = Spv.VEC4
+            
+            allowed_types[token(idx + 1)] = [to_write, token(idx + 2)]
             
         if opcode == 24:  # OpTypeMatrix
-            allowed_types[spv[idx + 1]] = [f'mat{spv[idx + 3]}', spv[idx + 2]]
+            to_write = Spv.UNKNOWN
+            msz = token(idx + 3)
+            if msz == 2:
+                to_write = Spv.MAT2
+            elif msz == 3:
+                to_write = Spv.MAT3
+            elif msz == 4:
+                to_write = Spv.MAT4
+            
+            allowed_types[token(idx + 1)] = [to_write, token(idx + 2)]
+        
+        # if opcode == 25:  # OpTypeImage
+        # if opcode == 26:  # OpTypeSampler
+        # if opcode == 27:  # OpTypeSampledImage
+        #if opcode == 28:  # OpTypeArray
+        #    allowed_types[token(idx + 1)] = [f'arr{token(idx + 3)}', token(idx + 2)]
+            
+        # if opcode == 29:  # OpTypeRuntimeArray
+        # if opcode == 30:  # OpTypeStruct
+        # if opcode == 31:  # OpTypeOpaque
+        # if opcode == 33:  # OpTypeFunction
+        # if opcode == 34:  # OpTypeEvent
+        # if opcode == 35:  # OpTypeDeviceEvent
+        # if opcode == 36:  # OpTypeReserveId
+        # if opcode == 37:  # OpTypeQueue
+        # if opcode == 38:  # OpTypePipe
+        # if opcode == 39:  # OpTypeForwardPointer
+        
         
         idx += args
     ##################
@@ -435,7 +564,7 @@ def parse_spv_inputs(raw_spv: bytes):
         typ, thrw_typ = allowed_types[type_id]
         if thrw_typ != -1:
             add_typ = assembly(thrw_typ)
-            allowed_types[type_id][0] = add_typ+' '+typ
+            allowed_types[type_id][0] = add_typ | typ
             allowed_types[type_id][1] = -1
         return typ
 
@@ -450,79 +579,56 @@ def parse_spv_inputs(raw_spv: bytes):
     ###############
 
     # == Making a whole list == #
-    exrtacted_general_ids = [] # id, id, id ...
-    exrtacted_general_ids = list( # get all identifiers for variables
+    exrtacted_general_ids: List[int] = [] # id, id, id ...
+    exrtacted_general_ids: List[int] = list( # get all identifiers for variables
         set(extracted_location.keys()) | set(extracted_types.keys()) | \
         set(extracted_storage_class_id.keys()) | set(extracted_names.keys()))
     # exrtacted_general_ids.sort()  # if needed for sorting locations
-
-    extracted_collected = {}
+    
+    
+    extracted_collected: Dict[int, List[str, int, int, int]] = {}  # id : variable_info
     for ids in exrtacted_general_ids:
-        to_add = {'name':'', 'class':-1, 'type':'', 'location':-1}
+        to_add = ['', -1, -1, -1]  # name, class, type, location
         if ids in extracted_names:
-            to_add['name'] = extracted_names[ids]
+            to_add[0] = extracted_names[ids]
         if ids in extracted_storage_class_id:
-            to_add['class'] = extracted_storage_class_id[ids]
+            to_add[1] = extracted_storage_class_id[ids]
         if ids in extracted_types:
-            to_add['type'] = extracted_types[ids]
+            to_add[2] = extracted_types[ids]
         if ids in extracted_location:
-            to_add['location'] = extracted_location[ids]
+            to_add[3] = extracted_location[ids]
         extracted_collected[ids]=to_add
     ##################
 
     # == Conversion to the moderngl type == #
-    mgl_attr_table = {  # py_type : mgl_type
-        'i4': 0x1404,  # GL_INT
-        'i4 vec2': 0x8b53,  # GL_INT_VEC2
-        'i4 vec3': 0x8b54,  # GL_INT_VEC3
-        'i4 vec4': 0x8b55,  # GL_INT_VEC4
-        'ui4': 0x1405,  # GL_UNSIGNED_INT
-        'ui4 vec2': 0x8dc6,  # GL_UNSIGNED_INT_VEC2
-        'ui4 vec3': 0x8dc7,  # GL_UNSIGNED_INT_VEC3
-        'ui4 vec4': 0x8dc8,  # GL_UNSIGNED_INT_VEC4
-        'f4': 0x1406,  # GL_FLOAT
-        'f4 vec2': 0x8b50,  # GL_FLOAT_VEC2
-        'f4 vec3': 0x8b51,  # GL_FLOAT_VEC3
-        'f4 vec4': 0x8b52,  # GL_FLOAT_VEC4
-        'f8': 0x140a,  # GL_DOUBLE
-        'f8 vec2': 0x8ffc,  # GL_DOUBLE_VEC2
-        'f8 vec3': 0x8ffd,  # GL_DOUBLE_VEC3
-        'f8 vec4': 0x8ffe,  # GL_DOUBLE_VEC4
-        'f4 vec2 mat2': 0x8b5a,  # GL_FLOAT_MAT2
-        'f4 vec3 mat2': 0x8b65,  # GL_FLOAT_MAT2x3
-        'f4 vec4 mat2': 0x8b66,  # GL_FLOAT_MAT2x4
-        'f4 vec2 mat3': 0x8b67,  # GL_FLOAT_MAT3x2
-        'f4 vec3 mat3': 0x8b5b,  # GL_FLOAT_MAT3
-        'f4 vec4 mat3': 0x8b68,  # GL_FLOAT_MAT3x4
-        'f4 vec2 mat4': 0x8b69,  # GL_FLOAT_MAT4x2
-        'f4 vec3 mat4': 0x8b6a,  # GL_FLOAT_MAT4x3
-        'f4 vec4 mat4': 0x8b5c,  # GL_FLOAT_MAT4
-        'f8 vec2 mat2': 0x8f46,  # GL_DOUBLE_MAT2
-        'f8 vec3 mat2': 0x8f49,  # GL_DOUBLE_MAT2x3
-        'f8 vec4 mat2': 0x8f4a,  # GL_DOUBLE_MAT2x4
-        'f8 vec3 mat2': 0x8f4b,  # GL_DOUBLE_MAT3x2
-        'f8 vec3 mat3': 0x8f47,  # GL_DOUBLE_MAT3
-        'f8 vec4 mat3': 0x8f4c,  # GL_DOUBLE_MAT3x4
-        'f8 vec2 mat4': 0x8f4d,  # GL_DOUBLE_MAT4x2
-        'f8 vec3 mat4': 0x8f4e,  # GL_DOUBLE_MAT4x3
-        'f8 vec4 mat4': 0x8f48,  # GL_DOUBLE_MAT4
-    }
-
-    extracted_collected_end = {}
     for ids, item in extracted_collected.items():
-        to_add = {'name':item['name'], 'class':item['class'], 'type':-1, 'location':item['location']}
-        if item['type'] != '':
-            if str(item['type']) not in mgl_attr_table:
+        if item[2] != -1:
+            if item[2] not in TRANSLATION_TABLE_SPIRV_GLSL:
                 raise RuntimeError(f"Could not find the encoding of the variable type \"{item['type']}\".")
-            to_add['type'] = mgl_attr_table[item['type']]
-        extracted_collected_end[ids] = to_add
+            item[2] = TRANSLATION_TABLE_SPIRV_GLSL[item[2]]
     ##################
+
+
+    # == Getting Results == #
+    # Here you can trim the content to the required class.
+    """
+    print('== Input ==')
+    for key, item in extracted_collected.items():
+        # print(key, item)
+        if item[1] == 1 and item[3] != -1:  # Input storage class
+            print('---|', item[0], hex(item[2]), item[3])
+    print('\n== Output ==')
+    for key, item in extracted_collected.items():
+        if item[1] == 3 and item[3] != -1:  # Output storage class
+            print('---|', item[0], hex(item[2]), item[3])
+    """
+    #################
     
     # == Cropping the data to the required output == #
-    result: Dict[int, Tuple[int, int, int, int, bool, str]] = {}
-    for key, item in extracted_collected_end.items():
-        if item['class'] == 1 and item['location'] != -1:
-            result[item['location']] = ATTRIBUTE_LOOKUP_TABLE.get(item['type'], 
+    result:Dict[int, List[int, int, int, int, bool, str]]  = {}
+    for key, item in extracted_collected.items():
+        if item[1] == 1 and item[3] != -1:
+            result[item[3]] = ATTRIBUTE_LOOKUP_TABLE.get(item[2], 
                                             (1, 0, 1, 1, False, '?'))
     #########################
     
