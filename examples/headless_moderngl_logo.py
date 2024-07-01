b32b3a6e8a6f6cceec8a2d4120dd27681a495b2e
Szabolcs Dombi <szabolcs@szabolcsdombi.com>
2024-07-01 21:42:57 +0300

    upadte headless example

--- examples/headless_moderngl_logo.py
+++ examples/headless_moderngl_logo.py
@@ -3,8 +3,8 @@ from PIL import Image
 
 ctx = moderngl.create_standalone_context()
 
-fbo1 = ctx.framebuffer([ctx.renderbuffer((800, 800), components=4, samples=16)])
-fbo2 = ctx.framebuffer([ctx.renderbuffer((800, 800), components=4)])
+fbo = ctx.framebuffer([ctx.renderbuffer((800, 800), components=4, samples=16)])
+output = ctx.framebuffer([ctx.renderbuffer((800, 800), components=4)])
 
 prog = ctx.program(
     vertex_shader='''
@@ -42,14 +42,14 @@ prog = ctx.program(
 )
 
 vao = ctx.vertex_array(prog, [])
-vao.scope = ctx.scope(fbo1)
 vao.instances = 9
 vao.vertices = 3
 
-fbo1.clear(0.0, 0.0, 0.0, 0.0)
+fbo.use()
+ctx.clear()
 vao.render()
 
-ctx.copy_framebuffer(fbo2, fbo1)
+ctx.copy_framebuffer(output, fbo)
 
-img = Image.frombuffer('RGBA', fbo2.size, fbo2.read(components=4)).transpose(Image.Transpose.FLIP_TOP_BOTTOM)
+img = Image.frombuffer('RGBA', output.size, output.read(components=4)).transpose(Image.Transpose.FLIP_TOP_BOTTOM)
 img.save('logo.png')
